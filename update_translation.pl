99e3ffc3214d1cc39bc0783653161e97436d365c
mgruner <mgruner>
2003-11-15 18:51:43 +0000

    changing i18n system to only use 1 po file

--- update_translation.pl
+++ update_translation.pl
@@ -30,92 +30,43 @@ sub update_pot_files() {
 		my $command = "xml2pot $source/$file > $potfile";
 		`$command`;
 
-		print "\tCreating POT $potfile\n";
+		print "\tCreating temporary POT $potfile\n";
 	}
 
+	print "Merging POT files.";
+	`msgcat $dest/*.pot > $dest/full.pot.cat`;
+	`rm $dest/*.pot`;
+	move("$dest/full.pot.cat","$dest/full.pot");
 
 	closedir(DIR);
 }
 
 
-# 1st parameter is the dir with the english XML sources
+# 1st parameter is the dir where the POT files are
 # 2nd parameter is the dir where the PO files should be put
-# 3rd parameter is the dir where the POT files are
-# 4th parameter is the language name
 sub update_po_files() {
-	my $source = shift;
-	my $dest = shift;
 	my $potdir = shift;
-	my $lang = shift;
+	my $podir  = shift;
 
-	print "Making PO files for $lang\n";
-	mkpath($dest);
+	print "Making PO file\n";
+	mkpath($podir);
 
-	opendir(DIR, $source);
+	my $potfile = "$potdir/full.pot";
+	my $pofile  = "$podir/full.po";
 
-	while (my $file = readdir(DIR)) {
-		next unless ($file =~ /(\.xml|\.docbook)$/);
-		next if ($file eq "catalog.xml");
-		next if ($file eq "index.xml");
-		next if ($file eq "autolayout.xml");
+	`msgmerge --no-wrap $pofile $potfile -o $pofile.new`;
 
-		my $pofile = $dest . $file;
-		$pofile =~ s/(\.xml|\.docbook)$/\.po/;
-
-		print "\tCreating PO file $pofile\n";
-
-		my $potfile = $potdir . $file;
-		$potfile =~ s/(\.xml|\.docbook)$/\.pot/;
-
-		if (-e $pofile) { # PO file exists already
-			my $command = "msgmerge --no-wrap $pofile $potfile -o $pofile.new > /dev/null 2>&1";
-			`$command`;
-
-			if (compare($pofile, "$pofile.new") != 0) { #different
-			#if (!compare($pofile, "$pofile.new")) { #different
-				move("$pofile.new", $pofile);
-				print "\t\tMerged in changes\n";
-			}
-			else {
-				print "\t\tSame entries!\n";
-			}
-			unlink("$pofile.new");
-		}
-		else { #po file not yet there
-			print "\t\tCopied file\n";
-			copy($potfile, $pofile);
-
-			#merge in the already translated entries of our previous webpages!
-			# We use all files in compendium/$lang/*.po
-			opendir(C_DIR, "$source/../compendium/$lang");
-			my $compendium_files;
-			my $first_compendium;
-			while (my $compendium_file = readdir(C_DIR)) {
-				next unless ($compendium_file =~ /\.po$/);
-				if (!$first_compendium){
-					$first_compendium = "$source/../compendium/$lang/$compendium_file";
-					next;
-				}
-				$compendium_files .= " -C $source/../compendium/$lang/$compendium_file";
-			}
-			closedir(C_DIR);
-
-			copy("$pofile", "$pofile-old");
-			my $command = "msgmerge --no-wrap -D $source/../compendium/$lang $compendium_files --output=$pofile-tmp $first_compendium $pofile-old > /dev/null 2>&1";
-			`$command`;
-
-			$command = "msgcomm --no-wrap --more-than=1 --output=$pofile $pofile-tmp $pofile-old > /dev/null 2>&1";
-			`$command`;
-
-			unlink("$pofile-old");
-			unlink("$pofile-tmp");
-			print "\t\tMerged in old translations from $compendium_files\n\n";
-		}
+	if (compare($pofile, "$pofile.new") != 0) { #different
+		move("$pofile.new", $pofile);
+		print "\t\tMerged in changes\n";
 	}
+	else {
+		print "\t\tSame entries!\n";
+	}
+	unlink("$pofile.new");
 
 	print "\n\n";
 
-	closedir(DIR);
 }
 
 # 1st parameter is the dir with the english sources
@@ -126,6 +77,8 @@ sub make_xml_files() {
 	my $podir = shift;
 	my $dest = shift;
 
+	my $pofile = "$podir/full.po";
+
 	print "Creating XML files\n";
 	opendir(DIR, $source);
 
@@ -133,38 +86,10 @@ sub make_xml_files() {
 		next unless ($file =~ /(\.xml|\.docbook)$/);
 		next if ($file eq "catalog.xml");
 
-		my $pofile = $podir . $file;
-		$pofile =~ s/(\.xml|\.docbook)$/\.po/;
-
-		# Mow fill into the empty msgstr fields the original english msgid
-		# po2xml leavesslated entries out, that's not what we want
-
-		#work on $pofile-full and fill in english translations
-		copy("$pofile","$pofile-full");
-		my $aref = Locale::PO->load_file_asarray("$pofile");
-		my @entries = @$aref if ($aref);
-		print "$pofile: " . ($#entries+1) ."\n";
-		if ($#entries+1) {
-			foreach my $entry (@entries) {
-				#print "copied " . $entry->msgid() . "\n" if ($entry->msgstr());;
-				my $msgid = $entry->dequote( $entry->msgid() );
-				my $msgstr = $entry->dequote( $entry->msgstr() );
-				my $no_msgstr = !$msgstr || ($msgstr eq "") || (length($msgstr) == 0);
-				$entry->msgstr($msgid) if ($no_msgstr);
-			}
-		}
-		die unless Locale::PO->save_file_fromarray("$pofile-full", \@entries);
-
-		my $xmlfile = $dest . $file;
-
-		print "\tCreating XML file $xmlfile\n";
-
-		my $originalXML = "$source/$file";
-		my $command = "po2xml $originalXML $pofile-full > $xmlfile";
-		`$command`;
-
-		unlink("$pofile-full");
+		print "\tCreating XML file $file\n";
+		`po2xml $source/$file $pofile > $dest/$file`;
 	}
+
 	print "\n\n";
 
 	closedir(DIR);
@@ -199,7 +124,7 @@ while ($lang = shift(@ARGV)) {
 	}
 
 	print "Working on $lang ...\n";
-	&update_po_files($ENV{"PWD"}. "/en",  $ENV{"PWD"} . "/$lang/po/", $ENV{"PWD"} . "/en/pot/",$lang);
+	&update_po_files($ENV{"PWD"} . "/en/pot/", $ENV{"PWD"} . "/$lang/po/");
 	&make_xml_files($ENV{"PWD"}. "/en", $ENV{"PWD"} . "/$lang/po/", $ENV{"PWD"} . "/$lang/");
 	&make_makefile("$lang");
 	&run_make("$lang");
