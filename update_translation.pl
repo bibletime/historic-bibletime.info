c02365094dc7ff6995fc594140ec1af38ea20d88
joachim <joachim>
2003-11-16 20:11:10 +0000

    fixes to the update script; readded /blue.css; removed plain.css and */*.css; fixes

--- update_translation.pl
+++ update_translation.pl
@@ -66,7 +66,6 @@ sub update_po_files() {
 	unlink("$pofile.new");
 
 	print "\n\n";
-
 }
 
 # 1st parameter is the dir with the english sources
@@ -76,22 +75,39 @@ sub make_xml_files() {
 	my $source = shift;
 	my $podir = shift;
 	my $dest = shift;
-
 	my $pofile = "$podir/full.po";
 
 	print "Creating XML files\n";
-	opendir(DIR, $source);
 
+	# Create a PO file which contains the original english strings as msgstr of empty/untranslated entries
+	# This way the files won"t be empty if the translation is misisng
+	copy("$pofile","$pofile-complete");
+	my $aref = Locale::PO->load_file_asarray("$pofile");
+	my @entries = @$aref if ($aref);
+	if ($#entries+1) {
+		foreach my $entry (@entries) {
+			my $msgid = $entry->dequote( $entry->msgid() );
+			my $msgstr = $entry->dequote( $entry->msgstr() );
+			my $no_msgstr = !$msgstr || ($msgstr eq "") || (length($msgstr) == 0);
+			$entry->msgstr($msgid) if ($no_msgstr);
+		}
+	}
+	die unless Locale::PO->save_file_fromarray("$pofile-complete", \@entries);
+
+
+	opendir(DIR, $source);
 	while (my $file = readdir(DIR)) {
 		next unless ($file =~ /(\.xml|\.docbook)$/);
 		next if ($file eq "catalog.xml");
 
 		print "\tCreating XML file $file\n";
-		`po2xml $source/$file $pofile > $dest/$file`;
+		`po2xml $source/$file $pofile-complete > $dest/$file`;
 	}
 
-	print "\n\n";
+	# Delete temporary PO file used for XML file creation
+	unlink("$pofile-complete");
 
+	print "\n\n";
 	closedir(DIR);
 }
 
